09d47cc28ee0c59727b1c292a2d095920b58655a
ptakpatryk <ptaszqq@gmail.com>
2022-10-22 15:59:03 +0100

    treesitter autotag

--- lua/ptak/plugins.lua
+++ lua/ptak/plugins.lua
@@ -59,7 +59,7 @@ return packer.startup(function(use)
   -- use({ "ahmedkhalf/project.nvim", commit = "541115e762764bc44d7d3bf501b6e367842d3d4f" })
   -- use({ "lewis6991/impatient.nvim", commit = "969f2c5c90457612c09cf2a13fee1adaa986d350" })
   -- use({ "lukas-reineke/indent-blankline.nvim", commit = "6177a59552e35dfb69e1493fd68194e673dc3ee2" })
-  -- use({ "goolord/alpha-nvim", commit = "ef27a59e5b4d7b1c2fe1950da3fe5b1c5f3b4c94" })
+  use({ "goolord/alpha-nvim" })
   -- use("folke/which-key.nvim")
 
   -- Colorschemes
@@ -88,6 +88,7 @@ return packer.startup(function(use)
   -- Treesitter
   use({ "nvim-treesitter/nvim-treesitter" })
   use({ "nvim-treesitter/nvim-treesitter-context" })
+  use({ "windwp/nvim-ts-autotag" })
 
   -- Git
   use({ "lewis6991/gitsigns.nvim" })
