60663e1b2031bcb037391159cfec7d6113347477
Patryk Ptak <ptaszqq@gmail.com>
2023-11-30 07:54:00 +0000

    incremental selection treesitter

--- lua/ptak/treesitter.lua
+++ lua/ptak/treesitter.lua
@@ -1,20 +1,29 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  return
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
-	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = { enable = true, disable = { "python", "css" } },
+  ensure_installed = "all", -- one of "all" or a list of languages
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true,         -- false will disable the whole extension
+    disable = { "css" },   -- list of language that will be disabled
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = { enable = true, disable = { "python", "css" } },
   autotag = {
     enable = true,
-  }
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = false,
+      node_decremental = "grm",
+    },
+  },
 })
